d1c43c24fa2179c777afa486abcfda1254df7990
Yago Frankley <frankleyyago@outlook.com>
2023-10-07 16:26:48 -0300

    feat: show a task dialog message

--- Labs/MyIntroCs/1_HelloWorld.cs
+++ Labs/MyIntroCs/1_HelloWorld.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
 
 namespace MyIntroCs
 {
-    public class HelloWorld
+    /// <summary>
+    /// Hello World #1 - A minimum Revit external command.
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.ReadOnly)]
+    public class HelloWorld : Autodesk.Revit.UI.IExternalCommand
     {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Autodesk.Revit.UI.TaskDialog.Show("My Dialog Title", "Hello World!");
+
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
     }
 }
