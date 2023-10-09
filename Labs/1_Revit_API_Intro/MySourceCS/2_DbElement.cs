9b746145be9700d513ef936429e6c75fb777b6a5
Yago Frankley <frankleyyago@outlook.com>
2023-10-08 21:50:55 -0300

    feat: identify type and show parametrs

--- Labs/1_Revit_API_Intro/MySourceCS/2_DbElement.cs
+++ Labs/1_Revit_API_Intro/MySourceCS/2_DbElement.cs
@@ -11,49 +11,60 @@ using Autodesk.Revit.UI.Selection;
 
 namespace MyIntroCs
 {
-    #region DbElement
     /// <summary>
-    /// DB Element - Retrieve element infos
+    /// DB Element - Retrieve element infos.
     /// </summary>
     [Transaction(TransactionMode.Manual)]
     internal class DbElement : IExternalCommand
     {
-        //Member variables
-        Application app;
-        Document doc;
+        //Member variables.
+        Application _app;
+        Document _doc;
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            //Get the access to the top most objects
+            //Get the access to the top most objects.
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
-            app = uiapp.Application;
-            doc = uidoc.Document;
-
-            //Pick an element on a screen
-            Reference pickedObj = uidoc.Selection.PickObject(ObjectType.Element, "Pick an element");
+            _app = uiapp.Application;
+            _doc = uidoc.Document;
+            
+            //Pick an element on a screen.
+            Reference r = uidoc.Selection.PickObject(ObjectType.Element, "Pick an element");
 
             //Get an element
-            Element elem = doc.GetElement(pickedObj);
+            Element e = _doc.GetElement(r);
 
-            //Call a method to show infos
-            ShowBasicElementInfo(elem);
+            //Call a method to show infos.
+            ShowBasicElementInfo(e);
+
+            //Call a method to identify element type.
+            IdentifyElement(e);
+
+            //Call a method to show parameters
+            ShowParameters(e, "Element Parameters");
+
+            ElementId eTypeId = e.GetTypeId();
+            ElementType eType = (ElementType)_doc.GetElement(eTypeId);
+            ShowParameters(eType, "Type Parameters");
 
             return Result.Succeeded;
         }
 
-        //Method to retrieve some elements info
-        public void ShowBasicElementInfo(Element elem)
+        /// <summary>
+        /// Show basic information about the given element.
+        /// </summary>
+        public void ShowBasicElementInfo(Element e)
         {
             //Element infos
             string s = "You picked:\n";
 
-            s += $"  Class name = {elem.GetType().Name}\n";
-            s += $"  Category = {elem.Category.Name}\n";
-            s += $"  Element id = {elem.Id.ToString()}\n\n";
+            s += $"  Class name = {e.GetType().Name}\n";
+            s += $"  Category = {e.Category.Name}\n";
+            s += $"  Element id = {e.Id.ToString()}\n\n";
 
             //Type info
-            ElementId elemTypeId = elem.GetTypeId();
-            ElementType elemType = (ElementType)doc.GetElement(elemTypeId);
+            ElementId elemTypeId = e.GetTypeId();
+            ElementType elemType = (ElementType)_doc.GetElement(elemTypeId);
 
             s += $"It's ElementType:\n";
             s += $"  Class name = {elemType.GetType().Name}\n";
@@ -61,7 +72,123 @@ namespace MyIntroCs
             s += $"  Element type id = {elemType.Id.ToString()}\n";
 
             TaskDialog.Show("Basic Element Info", s);
-        }        
+        }
+
+        /// <summary>
+        /// Identify the type of the element.
+        /// </summary>
+        public void IdentifyElement(Element e)
+        {
+            //An instance of a system family has a designated class
+            //We can use this to identify the type of element
+            //e.g., walls, floors, roofs
+            string s = " ";
+            if (e is Wall)
+            {
+                s = "Wall";
+            }
+            else if (e is Floor)
+            {
+                s = "Floor";
+            }
+            else if (e is RoofBase)
+            {
+                s = "Roof";
+            }
+            else if (e is FamilyInstance)
+            {
+                //An instance of a component family is all FamilyInstance
+                //We'll need to further check it's category
+                //e.g., doors, windows, furniture
+                if (e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Doors)
+                {
+                    s = "Doors";
+                }
+                else if (e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Windows)
+                {
+                    s = "Windows";
+                }
+                else if (e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Furniture)
+                {
+                    s = "Furniture";
+                }
+                else
+                {
+                    //e.g., plant
+                    s = "Component family instance";
+                }
+            }
+            else if (e is HostObject)
+            {
+                //Check the base class
+                //e.g, CeilingAndFloor
+                s = "System family instance";
+            }
+            else
+            {
+                s = "Other";
+            }
+
+            s = $"You have picked: {s}";
+
+            TaskDialog.Show("Identify Element", s);
+        }
+
+        /// <summary>
+        /// Show all paramaters values of the element.
+        /// </summary>
+        public void ShowParameters(Element e, string header)
+        {
+            string s = string.Empty;
+
+            foreach (Parameter p in e.GetOrderedParameters())
+            {
+                string name = p.Definition.Name;
+                string val = ParameterToString(p);
+
+                s += $"{name} = {val}\n";
+            }
+
+            TaskDialog.Show(header, s);
+        }
+
+        /// <summary>
+        /// Return a string from of the given parameter.
+        /// </summary>
+        private string ParameterToString(Parameter p)
+        {
+            string val = "none";
+
+            if (p == null)
+            {
+                return val;
+            }
+
+            switch (p.StorageType)
+            {
+                case StorageType.Double:
+                    double dVal = p.AsDouble();
+                    val = dVal.ToString();
+                    break;
+                case StorageType.Integer:
+                    int iVal = p.AsInteger();
+                    val = iVal.ToString();
+                    break;
+                case StorageType.String:
+                    string sVal = p.AsString();
+                    val = sVal.ToString();
+                    break;
+                case StorageType.ElementId:
+                    ElementId eVal = p.AsElementId();
+                    val = eVal.IntegerValue.ToString();
+                    break;
+                case StorageType.None:
+                    break;
+                default:
+                    break;
+            }
+
+            return val;
+        }
     }
-    #endregion
 }
