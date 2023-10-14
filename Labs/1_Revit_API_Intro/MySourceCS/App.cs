75fd612bbd1016b75ff831e96a9f02d6861c6e3e
Yago Frankley <frankleyyago@outlook.com>
2023-10-13 21:38:45 -0300

    feat: create a tab and pushbuttons

---
+++ Labs/1_Revit_API_Intro/MySourceCS/App.cs
@@ -0,0 +1,114 @@
+﻿#region Namespace
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+#endregion
+
+namespace MyIntroCs
+{
+    /// <summary>
+    /// Implements the Revit add-in interface IExternalApplication
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class App : IExternalApplication
+    {
+        /// <summary>
+        /// Implements the on Shutdown event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Implements the OnStartup event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if (panel.AddItem(new PushButtonData("FirstPlugin1", "FirstPlugin2", thisAssemblyPath, "MyIntro.Command"))
+                is PushButton button1)
+            {
+                button1.ToolTip = "My First Plugin";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "Icon.ico"));
+                BitmapImage bitmapImage = new BitmapImage(uri);
+                button1.LargeImage = bitmapImage;
+
+            }
+
+            panel.AddSeparator();
+
+            if (panel.AddItem(new PushButtonData("FirstPlugin2", "FirstPlugin3", thisAssemblyPath, "MyIntro.2_DbElement"))
+                is PushButton button2)
+            {
+                button2.ToolTip = "My First Plugin";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "Icon.ico"));
+                BitmapImage bitmapImage = new BitmapImage(uri);
+                button2.LargeImage = bitmapImage;
+
+            }
+
+            return Result.Succeeded;
+
+        }
+
+        #region RibbonPanel
+        /// <summary>
+        /// Function that creates RibbonPanel
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tab = "TabName";
+
+            RibbonPanel ribbonPanel = null;
+
+            try
+            {
+                a.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                RibbonPanel panel = a.CreateRibbonPanel(tab, "TabName");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            List<RibbonPanel> panels = a.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "TabName"))
+            {
+                ribbonPanel = p;
+            }
+
+            return ribbonPanel;
+
+
+        }
+        #endregion
+    }
+
+}
