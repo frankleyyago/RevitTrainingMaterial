bbbf4ce9e79afec7870f17b3f34a74b22419371f
Yago Frankley <frankleyyago@outlook.com>
2023-10-17 21:47:10 -0300

    feat: filter elements instances

--- Labs/1_Revit_API_Intro/MySourceCS/3_ElementFiltering.cs
+++ Labs/1_Revit_API_Intro/MySourceCS/3_ElementFiltering.cs
@@ -30,40 +30,74 @@ namespace MyIntroCs
             return Result.Succeeded;
         }
 
+        #region ListFamilyType()
+        /// <summary>
+        /// Filter and list elements (walls and doors).
+        /// </summary>
         public void ListFamilyTypes()
         {
 
             //Filter the document looking for element of type: WallType.
-            FilteredElementCollector wCollector = new FilteredElementCollector(_doc);
-            wCollector.WherePasses(new ElementClassFilter(typeof(WallType)));
+            FilteredElementCollector wCollector = new FilteredElementCollector(_doc)
+                .WherePasses(new ElementClassFilter(typeof(WallType)));
 
             //Create a list of elements with filtered elements, in this casse walls.
             IList<Element> wallTypes = wCollector.ToElements();
 
             //Filter the document looking for elements of type: FamilySymbol and category: OST_Doors.
-            FilteredElementCollector dCollector = new FilteredElementCollector(_doc);
-            dCollector.OfClass(typeof(FamilySymbol));
-            dCollector.OfCategory(BuiltInCategory.OST_Doors);
+            FilteredElementCollector dCollector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Doors);
 
             //Create a list of elements with filtered elements, in this case doors.
             IList<Element> doorTypes = dCollector.ToElements();
 
+            //Filter the document looking for elements of type: OST_Window.
+            FilteredElementCollector wiCollector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows);
+
+            //Create a list of elements with filtered elements, in this case windows.
+            IList<Element> windowTypes = wiCollector.ToElements();
+
+            //Filter the document looking for elements that are places in the model of type: OST_Doors.
+            FilteredElementCollector doorCollector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(FamilyInstance))
+                .OfCategory(BuiltInCategory.OST_Doors);
+
+            //Create a list of elements with filtered elements, in this case doors.
+            IList<Element> doorInstances = doorCollector.ToElements();
+
+
             ShowElementList(wallTypes, "Walls types (by filter): ");
             ShowElementList(doorTypes, "Doors types (by filter): ");
+            ShowElementList(windowTypes, "Windows types (by filter): ");
+            ShowElementList(doorInstances, "Door Instances (by filter): ");
         }
 
+        /// <summary>
+        /// Helper function 1: Show a list of elements (walls and doors).
+        /// </summary>
+        /// <param name="e">elements</param>
+        /// <param name="h">header</param>
         public void ShowElementList(IList<Element> e, string h)
         {
             string s = " - Class - Category - Name (or Family: Type Name) - Id - \r\n";
-
+            int num = 1;
             foreach (Element el in e)
             {
-                s += ElementToString(el);
+                s += $"{num} - {ElementToString(el)}";
+                num++;
             }
 
             TaskDialog.Show($"{h} ({e.Count.ToString()}):", s);
         }
 
+        /// <summary>
+        /// Helper function 2: Summarize elements informatin as a line of text.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
         public string ElementToString(Element e)
         {
             if (e == null)
@@ -88,5 +122,6 @@ namespace MyIntroCs
 
             return $"{e.GetType().Name}; {e.Category.Name}; {name}; {e.Id.IntegerValue.ToString()}\r\n";
         }
+        #endregion
     }
 }
