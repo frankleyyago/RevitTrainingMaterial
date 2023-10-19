7ed088adf07e19b1b2e399e7eb3581fefe496aac
Yago Frankley <frankleyyago@outlook.com>
2023-10-18 21:23:20 -0300

    feat: find wall family type

--- Labs/1_Revit_API_Intro/MySourceCS/3_ElementFiltering.cs
+++ Labs/1_Revit_API_Intro/MySourceCS/3_ElementFiltering.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI.Selection;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 #endregion
 
 namespace MyIntroCs
@@ -26,6 +27,8 @@ namespace MyIntroCs
             _doc = uidoc.Document;
 
             ListFamilyTypes();
+
+            FindFamilyType_Wall_v2("Basic Wall: ", "Test");
 
             return Result.Succeeded;
         }
@@ -68,11 +71,10 @@ namespace MyIntroCs
             //Create a list of elements with filtered elements, in this case doors.
             IList<Element> doorInstances = doorCollector.ToElements();
 
-
             ShowElementList(wallTypes, "Walls types (by filter): ");
             ShowElementList(doorTypes, "Doors types (by filter): ");
             ShowElementList(windowTypes, "Windows types (by filter): ");
-            ShowElementList(doorInstances, "Door Instances (by filter): ");
+            ShowElementList(doorInstances, "Door Instances (by filter): ");            
         }
 
         /// <summary>
@@ -121,6 +123,79 @@ namespace MyIntroCs
             }
 
             return $"{e.GetType().Name}; {e.Category.Name}; {name}; {e.Id.IntegerValue.ToString()}\r\n";
+        }
+        #endregion
+
+        #region FindFamilyType_Wall_v1()
+        /// <summary>
+        /// Find a specific family type for a wall with a given family and type name. Use LINQ.
+        /// </summary>
+        /// <param name="wFamilyName">Family name of the desired wall</param>
+        /// <param name="wTypeName">Type name of the desired wall</param>
+        /// <returns></returns>
+        public Element FindFamilyType_Wall_v1(string wFamilyName, string wTypeName)
+        {
+            //Narrow down a collector by class.
+            FilteredElementCollector wCollector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(WallType));
+
+            //Use LINQ query.
+            var wTypeElems =
+                from element in wCollector
+                where element.Name.Equals(wTypeName)
+                select element;
+
+            //Get the result.
+            Element wType = null;
+
+            //Create a list of elements to get the result of the query.
+            IList<Element> wTypeList = wTypeElems.ToList();
+            if (wTypeList.Count > 0)
+            {
+                wType = wTypeList[0];
+            }
+
+            return wType;
+        }
+        #endregion
+
+        #region FindFamilyType_Wall_v2()
+        /// <summary>
+        /// Find a specific family type for a wall with a given family and type name. Use iteration.
+        /// </summary>
+        /// <param name="wFamilyName"></param>
+        /// <param name="wTypeName"></param>
+        /// <returns></returns>
+        public Element FindFamilyType_Wall_v2(string wFamilyName, string wTypeName)
+        {
+            //Narrow down the collector by class.
+            FilteredElementCollector wCollector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(WallType));
+
+            //Use iterator.
+            FilteredElementIterator wTypeItr = wCollector.GetElementIterator();
+            wTypeItr.Reset();
+
+            Element wType = null;
+
+            while (wTypeItr.MoveNext())
+            {
+                WallType wallType = (WallType)wTypeItr.Current;
+                //Check two names: type name and family name.
+                if ((wallType.Name == wTypeName) & (wallType.get_Parameter(BuiltInParameter.SYMBOL_FAMILY_NAME_PARAM).AsString().Equals(wFamilyName)))
+                { 
+                    wType = wallType;
+                    TaskDialog.Show("Title", "I found your wall!");
+                    break;
+                }
+                else
+                {
+                    TaskDialog.Show("Title", "I don't found your wall!");
+                    break;
+                }
+            }
+
+            return wType;
         }
         #endregion
     }
