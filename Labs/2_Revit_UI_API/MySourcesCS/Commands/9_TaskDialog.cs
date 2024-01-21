37b2d386eab7ace1c54a59353f69ac1158b0ebc8
Yago Frankley <frankleyyago@outlook.com>
2024-01-21 19:23:36 -0300

    feat: creates a instance of task dialog

--- Labs/2_Revit_UI_API/MySourcesCS/Commands/9_TaskDialog.cs
+++ Labs/2_Revit_UI_API/MySourcesCS/Commands/9_TaskDialog.cs
@@ -28,19 +28,64 @@ namespace MyUiCs
             _uidoc = _uiapp.ActiveUIDocument;
             _doc = _uidoc.Document;
 
-            //(1) Shows a simple task dialog box.
+            //(0) Shows a simple task dialog box.
             TaskDialog.Show("Task Dialog Static 1", "Main message");
 
-            //(2) Adding [Yes] [No] [Cancel] buttons.
+            //(1) Adding [Yes] [No] [Cancel] buttons.
             TaskDialogResult tdr = default(TaskDialogResult);
             tdr = TaskDialog.Show("Task Dialog Static 2", "Main message", (TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No | TaskDialogCommonButtons.Cancel));
 
-            //(3) Sets a default button.
+            //(2) Sets a default button.
             TaskDialogResult tdr2 = default(TaskDialogResult);
             TaskDialogResult defaultButton = TaskDialogResult.No;
             tdr2 = TaskDialog.Show("Task Dialog Static 3", "Main message", (TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No|TaskDialogCommonButtons.Cancel), defaultButton);
 
+            InstanceOfTaskDialog();
+
             return Result.Succeeded;
+        }
+
+        public void InstanceOfTaskDialog()
+        {
+            //(0) Create an instance of task dialog to set more options.
+            TaskDialog myTaskDialog = new TaskDialog("Revit UI Labs - Task Dialog Options");
+
+            //(1) Set icon to the top portin.
+            myTaskDialog.MainIcon = TaskDialogIcon.TaskDialogIconWarning;
+
+            //(2) Set main instruction.
+            myTaskDialog.MainInstruction = "Main instruction: This is Revit UI Lab 3 Task Dialog";
+
+            //(3) Set main content.
+            myTaskDialog.MainContent = "Main content: You can add detailed description here.";
+
+            //(4) Set the button area.
+            myTaskDialog.CommonButtons = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No | TaskDialogCommonButtons.Cancel;
+            myTaskDialog.DefaultButton = TaskDialogResult.Yes;
+
+            //(5) Set expanded content.
+            myTaskDialog.ExpandedContent = "Expanded content: The visibility of this portion is controled by Show/Hide button";
+
+            //(6) Set verification message.
+            myTaskDialog.VerificationText = "Verification: Do not show this message again.";
+
+            //(7) Set footer text.
+            myTaskDialog.FooterText = "Access: <a href=\"https://www.autodesk.com/developrevit\">Revit Developer Center</a>";
+
+            //(8) Add commands links.
+            myTaskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Command Link 1", "Description 1");
+            myTaskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Command Link 2", "Description 2");
+            myTaskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, "Command Link 3", "Description 3");
+            myTaskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink4, "Command Link 4", "Description 4");
+
+            //(9) Assign link to the CommandLink4.
+            TaskDialogResult res = myTaskDialog.Show();
+            if (TaskDialogResult.CommandLink4 == res)
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = "https://google.com";
+                process.Start();
+            }
         }
     }
 }
