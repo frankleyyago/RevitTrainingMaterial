20ab25710689f8f82f00ff0b8ae542ec7b7f1ddb
Yago Frankley <frankleyyago@outlook.com>
2024-01-21 19:55:17 -0300

    feat: event handling.

---
+++ Labs/2_Revit_UI_API/MySourcesCS/Commands/9_Event.cs
@@ -0,0 +1,71 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+#endregion
+
+namespace MyUiCs
+{
+    internal class UIEvent : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            WindowDoorUpdater winDoorUpdater = new WindowDoorUpdater(application.ActiveAddInId);
+
+            UpdaterRegistry.RegisterUpdater(winDoorUpdater);
+
+            ElementClassFilter wallFilter = new ElementClassFilter(typeof(Wall));
+            UpdaterRegistry.AddTrigger(winDoorUpdater.GetUpdaterId(), wallFilter, Element.GetChangeTypeGeometry());
+
+            return Result.Succeeded;
+        }
+    }
+
+    public class WindowDoorUpdater : IUpdater
+    {
+        UpdaterId m_updaterId = null;
+
+        public static bool m_updateActive = false;
+
+        public WindowDoorUpdater(AddInId id)
+        {
+            m_updaterId = new UpdaterId(id, new Guid("B9E701A7-D690-42AE-A647-5FE8906BEC42"));
+        }
+
+        public void Execute(UpdaterData data)
+        {
+            if (!m_updateActive) return;
+        }
+
+        public string GetAdditionalInformation()
+        {
+            return "Door/Window updater: keeps doors and windows at the center of walls.";
+        }
+
+        public ChangePriority GetChangePriority()
+        {
+            return ChangePriority.DoorsOpeningsWindows;
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            return m_updaterId;
+        }
+
+        public string GetUpdaterName()
+        {
+            return "Window/Door Updater";
+        }
+    }
+}
