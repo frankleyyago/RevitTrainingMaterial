86a548c75c82ee9b33089bccf4f3d1b991690abd
Yago Frankley <frankleyyago@outlook.com>
2024-01-27 15:11:58 -0300

    chore: set new project

---
+++ Labs/3_Revit_Family_API/MyFamilyCs/App.cs
@@ -0,0 +1,101 @@
+﻿#region Namespace
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+#endregion
+
+namespace MyIntroCs
+{
+    /// <summary>
+    /// Implements the Revit add-in interface IExternalApplication
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class App : IExternalApplication
+    {
+        /// <summary>
+        /// Implements the on Shutdown event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Implements the OnStartup event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if (panel.AddItem(new PushButtonData("FirstPlugin11", "FirstPlugin21", thisAssemblyPath, "MyFamilyCs.ColumnRectangle"))
+                is PushButton button1)
+            {
+                button1.ToolTip = "My First Plugin";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "Icon.ico"));
+                BitmapImage bitmapImage = new BitmapImage(uri);
+                button1.LargeImage = bitmapImage;
+
+            }
+
+            return Result.Succeeded;
+
+        }
+
+        #region RibbonPanel
+        /// <summary>
+        /// Function that creates RibbonPanel
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tab = "MyFamilyCs";
+
+            RibbonPanel ribbonPanel = null;
+
+            try
+            {
+                a.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                RibbonPanel panel = a.CreateRibbonPanel(tab, "MyFamilyCs");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            List<RibbonPanel> panels = a.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "MyFamilyCs"))
+            {
+                ribbonPanel = p;
+            }
+
+            return ribbonPanel;
+
+
+        }
+        #endregion
+    }
+
+}
---
+++ Labs/3_Revit_Family_API/MyFamilyCs/Commands/1_ColumnRectangle.cs
@@ -0,0 +1,26 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+#endregion
+
+namespace MyFamilyCs
+{
+    public class ColumnRectangle : IExternalCommand
+    {
+        //Member variables.
+        Application _apps;
+        Document _doc;
+        UIDocument _uidoc;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Hello", "Hello");
+
+            return Result.Succeeded;
+        }
+    }
+}
